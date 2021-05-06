36da36e3c752735236fa29ded91ef1a8754f8be9
solareenlo <solareenlo@gmail.com>
2021-05-07 03:51:01 +0900

    update: main

--- philo_one/main.c
+++ philo_one/main.c
@@ -6,11 +6,44 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 03:21:57 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/05/06 18:45:57 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/05/07 03:50:26 by tayamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_one.h"
+
+void	*_monitor_philos(void *var)
+{
+	t_arg			*args;
+	t_philo			**philos;
+	t_monitor		monitor;
+
+	monitor = *(t_monitor *)var;
+	args = monitor.args;
+	philos = monitor.philos;
+	return ((void *)(&(monitor.philos)[0]->id));
+}
+
+int	_run_monitor(t_monitor *monitor)
+{
+	void	*id;
+	size_t	now;
+
+	if (pthread_create(&monitor->thread, NULL, &_monitor_philos, monitor) != 0)
+		return (1);
+	if (pthread_join(monitor->thread, &id) != 0)
+		return (1);
+	if (pthread_mutex_lock(&monitor->m_message) != 0)
+		return (1);
+	if (id)
+	{
+		now = ft_get_time_usec();
+		printf("%ld %d died\n", now, *(int *)id);
+	}
+	if (pthread_mutex_unlock(&monitor->m_message) != 0)
+		return (1);
+	return (0);
+}
 
 int	main(int argc, char *argv[])
 {
@@ -29,6 +62,8 @@ int	main(int argc, char *argv[])
 	if (ft_set_monitor(&monitor, philos, &args))
 		return (1);
 	if (ft_run_philos(&philos, &monitor, args))
+		return (1);
+	if (_run_monitor(&monitor))
 		return (1);
 	free(forks);
 	ft_free_philos(&philos, args.number_of_philo);
