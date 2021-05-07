bcd2b225ada24597425cdd21371715cda1940c9e
solareenlo <solareenlo@gmail.com>
2021-05-08 04:25:09 +0900

    add: philo_one3

---
+++ philo_one3/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/08 03:50:49 by tayamamo          #+#    #+#             */
+/*   Updated: 2021/05/08 04:02:20 by tayamamo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_one.h"
+
+int	main(int argc, char *argv[])
+{
+	t_arg			args;
+
+	if (ft_set_args(&args, argc, argv))
+		return (1);
+	ft_put_args(args);
+	return (0);
+}
