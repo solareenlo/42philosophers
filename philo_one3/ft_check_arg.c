7f3ec9934ad4d28856193c2f5ad131fba8720fc9
solareenlo <solareenlo@gmail.com>
2021-05-11 12:32:45 +0900

    fix: ft_check_arg

--- philo_one3/ft_check_arg.c
+++ philo_one3/ft_check_arg.c
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 13:58:18 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/05/08 03:59:16 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/05/11 12:31:33 by tayamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,31 @@
 static int	_puterr_arg(void)
 {
 	ft_putstr_fd("error: arg\nUsage: ./philo_one 1 2 3 4 [5]\n", 2);
-	ft_putstr_fd("\t1: number_of_philosophers (1 ~ 1000)\n", 2);
-	ft_putstr_fd("\t2: time_to_die (0 ~ 2147483647)\n", 2);
-	ft_putstr_fd("\t3: time_to_eat (0 ~ 2147483647)\n", 2);
-	ft_putstr_fd("\t4: time_to_sleep (0 ~ 2147483647)\n", 2);
-	ft_putstr_fd("\t5: [number_of_times_each_philosopher_must_eat", 2);
-	ft_putstr_fd(" (0 ~ 2147483647)]\n", 2);
+	ft_putstr_fd("\t 1 : number_of_philosophers (2 ~ ", 2);
+	ft_putnbr_fd(THREADSLIMIT, 2);
+	ft_putstr_fd(")\n", 2);
+	ft_putstr_fd("\t 2 : time_to_die (0 ~ ", 2);
+	ft_putnbr_fd(INTMAX, 2);
+	ft_putstr_fd(")\n", 2);
+	ft_putstr_fd("\t 3 : time_to_eat (0 ~ ", 2);
+	ft_putnbr_fd(INTMAX, 2);
+	ft_putstr_fd(")\n", 2);
+	ft_putstr_fd("\t 4 : time_to_sleep (0 ~ ", 2);
+	ft_putnbr_fd(INTMAX, 2);
+	ft_putstr_fd(")\n", 2);
+	ft_putstr_fd("\t[5]: number_of_times_each_philosopher_must_eat", 2);
+	ft_putstr_fd(" (0 ~ ", 2);
+	ft_putnbr_fd(INTMAX, 2);
+	ft_putstr_fd(")\n", 2);
 	return (1);
 }
 
 static int	_puterr_arg_1(void)
 {
 	ft_putstr_fd("Error: arg1\n", 2);
-	ft_putstr_fd("number_of_philosophers must be between 2 and 1000.\n", 2);
+	ft_putstr_fd("number_of_philosophers must be between 2 and ", 2);
+	ft_putnbr_fd(THREADSLIMIT, 2);
+	ft_putstr_fd(".\n", 2);
 	return (1);
 }
 
@@ -44,7 +56,9 @@ static int	_puterr_arg_n(int i)
 		ft_putstr_fd("time_to_sleep", 2);
 	else if (i == 5)
 		ft_putstr_fd("number_of_times_each_philosopher_must_eat", 2);
-	ft_putstr_fd(" must be between 0 and 2147483647.\n", 2);
+	ft_putstr_fd(" must be between 0 and ", 2);
+	ft_putnbr_fd(INTMAX, 2);
+	ft_putstr_fd(".\n", 2);
 	return (1);
 }
 
@@ -56,7 +70,7 @@ int	ft_check_arg(int argc, char *argv[])
 	if (argc < 5 || 6 < argc)
 		return (_puterr_arg());
 	tmp = ft_atoi(argv[1]);
-	if (ft_check_int(argv[1]) || tmp < 2 || 1000 < tmp)
+	if (ft_check_int(argv[1]) || tmp < 2 || THREADSLIMIT < tmp)
 		return (_puterr_arg_1());
 	i = 2;
 	while (i < argc)
