ea1bd31ea451094b771d6788dedbca98ff2f1054
solareenlo <solareenlo@gmail.com>
2021-05-11 12:19:14 +0900

    update: libft

--- philo_one3/libft1.c
+++ philo_one3/libft1.c
@@ -1,16 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isspace.c                                       :+:      :+:    :+:   */
+/*   libft1.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/08 15:07:00 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/05/04 04:00:33 by tayamamo         ###   ########.fr       */
+/*   Created: 2021/05/11 12:14:46 by tayamamo          #+#    #+#             */
+/*   Updated: 2021/05/11 12:15:29 by tayamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_one.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, 0, n);
+}
+
+void	*ft_calloc(size_t cnt, size_t size)
+{
+	void	*ptr;
+
+	if (cnt == 0 || size == 0)
+	{
+		cnt = 1;
+		size = 1;
+	}
+	ptr = malloc(cnt * size);
+	if (ptr)
+		ft_bzero(ptr, cnt * size);
+	return (ptr);
+}
+
+int	ft_isdigit(int c)
+{
+	return ('0' <= c && c <= '9');
+}
 
 int	ft_isspace(int c)
 {
@@ -27,4 +52,16 @@ int	ft_isspace(int c)
 	if (c == ' ')
 		return (1);
 	return (0);
+}
+
+void	*ft_memset(void *dst, int val, size_t n)
+{
+	unsigned char	*p;
+	unsigned char	c;
+
+	p = dst;
+	c = val;
+	while (n--)
+		*(p++) = c;
+	return (dst);
 }
--- philo_one3/libft2.c
+++ philo_one3/libft2.c
@@ -1,16 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   libft2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/08 17:12:01 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/05/04 15:03:14 by tayamamo         ###   ########.fr       */
+/*   Created: 2021/05/11 12:17:11 by tayamamo          #+#    #+#             */
+/*   Updated: 2021/05/11 12:17:58 by tayamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_one.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+	if (!c || fd < 0)
+		return ;
+	write(fd, &c, 1);
+}
+
+int	ft_puterr_malloc(void)
+{
+	ft_putstr_fd("Error: malloc failed\n", 2);
+	return (1);
+}
 
 void	ft_putnbr_fd(int n, int fd)
 {
@@ -29,4 +42,23 @@ void	ft_putnbr_fd(int n, int fd)
 		ft_putnbr_fd(nbr / 10, fd);
 	ft_putchar_fd((nbr % 10) + '0', fd);
 	return ;
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s || fd < 0)
+		return ;
+	write(fd, s, ft_strlen(s));
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (s == NULL)
+		return (i);
+	while (s[i])
+		i++;
+	return (i);
 }
