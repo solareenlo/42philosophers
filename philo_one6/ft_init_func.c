1c7b4705236043f7cb03b56774cccdc8123a56c0
solareenlo <solareenlo@gmail.com>
2021-05-12 21:37:05 +0900

    add: philo_one6

---
+++ philo_one6/ft_init_func.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_init_func.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/07 05:15:59 by tayamamo          #+#    #+#             */
+/*   Updated: 2021/05/12 21:32:08 by tayamamo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_one.h"
+
+int	ft_init_args(t_arg *args, int argc, char *argv[])
+{
+	if (ft_check_arg(argc, argv))
+		return (1);
+	args->number_of_philo = ft_atoi(argv[1]);
+	args->time_to_die = ft_atoi(argv[2]);
+	args->time_to_eat = ft_atoi(argv[3]);
+	args->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+	{
+		args->number_of_times_each_philo_must_eat = ft_atoi(argv[5]);
+		if (args->number_of_times_each_philo_must_eat == 0)
+			return (1);
+	}
+	else
+		args->number_of_times_each_philo_must_eat = 0;
+	return (0);
+}
